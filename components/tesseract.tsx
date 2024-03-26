ecd07209b17564ec7035530ada827462cac94b30
KaimuSeino <skim07a19yki@icloud.com>
2024-03-26 17:49:15 +0900

    tesseract: 画像を選択して日本語を抽出できるようにした

--- components/tesseract.tsx
+++ components/tesseract.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { createWorker } from "tesseract.js";
 
 const TesseractJS = () => {
@@ -8,26 +8,33 @@ const TesseractJS = () => {
   const [text, setText] = useState<string>("");
   const [image, setImage] = useState<string>("");
 
+  const handleChangeFile = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files?.length) return;
+    setImage(window.URL.createObjectURL(e.target.files[0]));
+    e.currentTarget.value = '';
+  }
+
   useEffect(() => {
     const recognizeImage = async () => {
-      const worker = await createWorker("eng", 1);
-      const { data: { text } } = await worker.recognize("/text.jpg")
+      const worker = await createWorker("jpn", 1);
+      const { data: { text } } = await worker.recognize(image);
       setText(text);
 
       await worker.terminate();
     };
-    recognizeImage()
-  }, [])
+    recognizeImage();
+  }, [image])
 
   return (
     <div className="mt-4 flex flex-col gap-2">
       画像から文字列を抽出
-
       <div>
-        <input type="file" name="" id="" />
+        <img className="border border-slate-700 p-4 m-2" src={image} alt="" />
+        <input type="file" name="image" id="" onChange={handleChangeFile} />
       </div>
 
       <div>
+        <h3 className="font-bold">抽出した文字</h3>
         {text}
       </div>
     </div>
